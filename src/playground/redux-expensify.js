3fcf220950cccb86101e6e99333770c77d66d511
armdong <armdong@163.com>
2018-03-07 15:02:18 +0800

    es6 spread operator in reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,31 @@
 import { createStore, combineReducers } from 'redux'
+import uuid from 'uuid'
 
 // ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+})
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id }) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+})
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -15,6 +39,13 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id)
     default:
       return state
   }
@@ -45,23 +76,8 @@ const store = createStore(
   })
 )
 
-console.log(store.getState())
+store.subscribe(() => console.log(store.getState()))
 
-const demoState = {
-  expenses: [
-    {
-      id: '01',
-      description: 'January Rent',
-      note: 'This was the final payment for that address',
-      amount: 54500,
-      createdAt: 0
-    }
-  ],
-  filters: {
-    text: 'rent',
-    sortBy: 'amount', // date or amount
-    startDate: undefined,
-    endDate: undefined
-  }
-}
-
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
+store.dispatch(removeExpense({ id: expenseOne.expense.id }))
