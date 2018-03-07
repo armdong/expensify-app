7f0808272b58bdd3ff392cd61bc138fac8b12c8d
armdong <armdong@163.com>
2018-03-07 16:05:21 +0800

    wrapping up our reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -40,9 +40,26 @@ const setTextFilter = (text = '') => ({
 })
 
 // SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+})
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+})
+
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+  type: 'SET_START_DATE',
+  startDate
+})
+
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+  type: 'SET_END_DATE',
+  endDate
+})
 
 // Exprense Reducer
 
@@ -78,6 +95,14 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
     case 'SET_TEXT_FILTER':
       return {...state, text: action.text}
+    case 'SORT_BY_DATE':
+      return {...state, sortBy: 'date'}
+    case 'SORT_BY_AMOUNT':
+      return {...state, sortBy: 'amount'}
+    case 'SET_START_DATE':
+      return {...state, startDate: action.startDate}
+    case 'SET_END_DATE':
+      return {...state, endDate: action.endDate}
     default:
       return state
   }
@@ -94,12 +119,19 @@ const store = createStore(
 
 store.subscribe(() => console.log(store.getState()))
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
+// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
+// const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
 
-store.dispatch(setTextFilter('rent'))
-store.dispatch(setTextFilter())
+// store.dispatch(setTextFilter('rent'))
+// store.dispatch(setTextFilter())
+
+// store.dispatch(sortByAmount())
+// store.dispatch(sortByDate())
+
+store.dispatch(setStartDate(125))
+store.dispatch(setStartDate())
+store.dispatch(setEndDate(1250))
