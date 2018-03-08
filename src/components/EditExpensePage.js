d40e7d9e7755eb9b4e4cfdf3b0d62f93fc169287
armdong <armdong@163.com>
2018-03-08 13:15:20 +0800

    wiring up edit expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import ExpenseForm from './ExpenseForm'
+import { editExpense, removeExpense } from '../actions/expenses'
 
-const EditExpensePage = ({
-  history,
-  location,
-  match
-}) => (
-  <div>
-    This is the expense with id of {match.params.id}.
-  </div>
-)
+const EditExpensePage = (props) => {
+  console.log(props.expense)
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense))
+          props.history.push('/')
+        }}
+      />
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }))
+          props.history.push('/')
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  )
+}
 
-export default EditExpensePage
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+})
+
+export default connect(mapStateToProps)(EditExpensePage)
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -5,13 +5,17 @@ import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 
 class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
-    canlendarFocused: false,
-    error: ''
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      canlendarFocused: false,
+      error: ''
+    }
   }
 
   onDescriptionChange = (e) => {
